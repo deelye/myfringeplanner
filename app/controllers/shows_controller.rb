1c9e72bac024fc44f94a129239bb60b0af1e7e61
Jahaira Castaneda <jahairacm@gmail.com>
2020-03-03 15:29:41 +0000

    creating PgSearch bar w/in show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,9 +1,20 @@
 class ShowsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
+  # def index
+  #   @shows = Show.all
   def index
-    @shows = Show.all
+    @shows = Show.where("title ILIKE ?", "%#{params[:query]}%")
   end
+  # if params[:query].present?
+  #   @search_term = params[:query]
+  #   @shows = Show.
+  # else
+  # end
+
+  #     @groups = @shows.group_by { |s| s.category }
+  #   end
+  # end
 
   def show
     @show = Show.find(params[:id])
--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,12 @@ class Show < ApplicationRecord
   belongs_to :venue
   has_many :shortlists
   validates :url, uniqueness: true
+
+ include PgSearch::Model
+     pg_search_scope :global_search,
+       against: [ :artist, :title, :description, :genre ],
+       using: {
+         tsearch: { prefix: true }
+       }
 end
+
