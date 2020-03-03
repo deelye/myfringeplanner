24eacdce87f8eaac5ba5f462066b9c2c9b401afd
deelye <dee.yen.lye@gmail.com>
2020-03-03 22:31:14 +0000

    centered and zoomed map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -16,12 +16,14 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
     const marker = JSON.parse(mapElement.dataset.marker);
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
 
-    fitMapToMarkers(map, markers);
+    map.setZoom(14);
+    map.setCenter([marker.lng, marker.lat])
     }
   };
 
