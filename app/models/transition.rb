8f4ad6aa395e0a4bf17b44e3f197651d9644e9f7
deelye <dee.yen.lye@gmail.com>
2020-03-05 10:46:30 +0000

    fixed association

--- app/models/transition.rb
+++ app/models/transition.rb
@@ -1,4 +1,4 @@
 class Transition < ApplicationRecord
   belongs_to :planner_to, class_name: 'Planner'
-  belongs_to :planner_from, class_nmae: 'Planner'
+  belongs_to :planner_from, class_name: 'Planner'
 end
