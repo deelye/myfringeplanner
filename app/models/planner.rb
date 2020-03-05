d39ff232c743108eac90527b7bc7c2e44dd1003e
deelye <dee.yen.lye@gmail.com>
2020-03-05 16:51:14 +0000

    lenny's work creating single day calendar and adding two events

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -3,4 +3,13 @@ class Planner < ApplicationRecord
   belongs_to :performance
   has_many :arriving_transitions, class_name: "Transition", foreign_key: :planner_to_id
   has_many :departing_transitions, class_name: 'Transition', foreign_key: :planner_from_id
+  before_save :set_top_and_duration
+
+  def set_top_and_duration
+    dur_in_5 = (self.performance.end - self.performance.start) / 300
+    top_in_5 = (self.performance.start - (self.performance.start.beginning_of_day + (3600 * 10))) / 300
+    self.duration = ((dur_in_5 / 168) * 100).round(2).to_s + "%"
+    self.top = ((top_in_5 / 168) * 100).round(2).to_s + "%"
+  end
+
 end
