0fcf551c0f972e1d7d6a178df5f1a487883cb913
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-03-06 17:22:31 +0000

    search working sort of, dates not quite working

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,16 +4,17 @@ class ShowsController < ApplicationController
 
   def index
     if params[:filter]
+      @shows = Show.all
       @start_date = ("#{params[:filter][:start_date]}/08/2020").to_datetime
       @end_date = ("#{params[:filter][:end_date]}/08/2020").to_datetime
       if params[:filter][:start_date].present? && params[:filter][:end_date].present?
         @shows = Performance.shows_between(@start_date, @end_date)
       end
       if params[:filter][:genre].present?
-        @shows = @shows.select{|r| r.genre = params[:filter][:genre]}
+        @shows = @shows.select{|r| r.genre == params[:filter][:genre]}
       end
       if params[:filter][:show].present?
-        @shows = @shows.select{|r| r.show.genre == params[:filter][:genre]}
+        @shows = @shows.select{|r| r.title.downcase.match?(params[:filter][:show].downcase)}
       end
     else
       @shows = Show.includes(:performances, :venue).all
