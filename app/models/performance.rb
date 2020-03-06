fd2a48f0b1e8dec0b9f8a2c75034940e45cb68da
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-03-06 15:51:35 +0000

    progress on creating ddate search functionality, but it's still broken.

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,4 +1,8 @@
 class Performance < ApplicationRecord
   belongs_to :show
   has_many :planners
+
+  def self.shows_between(start_date, end_date)
+    includes(:show).joins(:show).where("start >= ? and start <= ?", start_date, end_date ).map(&:show).uniq
+  end
 end
