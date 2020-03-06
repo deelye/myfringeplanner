3e5223a88f3397eb67964cfb8fd7fc2e768398b3
deelye <dee.yen.lye@gmail.com>
2020-03-06 16:55:02 +0000

    planner continued working with dynamic dates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,14 @@ class PagesController < ApplicationController
   end
 
   def planner
-    @day = "07/08/2020".to_datetime.day
+    @collection = ("01/08/2020".to_datetime.."31/08/2020".to_datetime).map{|r| r.to_date}
+    if params[:day].present?
+      @day = params[:day].to_datetime.day
+      @dayname = params[:day].to_datetime.strftime('%a-%d-%m')
+    else
+      @day = 1
+      @dayname = ("01/08/2020").to_datetime.strftime('%a-%d-%m')
+    end
     @planners = current_user.planners
     @raw_performances = current_user.all_follows.map{|r| r.followable.performances}.flatten.select{|r| r.start.day == @day}
     @booked_performance = current_user.planned_performances.select{|r| r.start.day == @day}
--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -6,14 +6,16 @@ class PlannersController < ApplicationController
   end
 
   def create
+    @day = params[:day]
     @performance = Performance.find(params[:performance_id])
     @planner = Planner.create!(user: current_user, performance: @performance)
-    redirect_to planner_path
+    redirect_to planner_path(day: @day)
   end
 
   def destroy
+    @day = params[:day]
     @planner = Planner.find(params[:id])
     @planner.destroy
-    redirect_to planner_path
+    redirect_to planner_path(day: @day)
   end
 end
