8c2339b6bb4ddef894eee4444263b142117d325d
deelye <dee.yen.lye@gmail.com>
2020-03-07 09:56:28 +0000

    replaced hardcoded 2020 with Time.now.year

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
   end
 
   def planner
-    @collection = ("01/08/2020".to_datetime.."31/08/2020".to_datetime).map{|r| r.to_date}
+    @collection = ("01/08/#{Time.now.year}".to_datetime.."31/08/#{Time.now.year}".to_datetime).map{|r| r.to_date}
     if params[:day].present?
       @day = params[:day].to_datetime.day
       @dayname = params[:day].to_datetime.strftime('%a-%d-%m')
