33d56434c49fcb95ae289ecf476509c21e32ef9a
deelye <dee.yen.lye@gmail.com>
2020-03-09 13:07:22 +0000

    performance won't display in shortlist if booked for another day

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -9,14 +9,14 @@ class PlannersController < ApplicationController
     @day = params[:day]
     @performance = Performance.find(params[:performance_id])
     @planner = Planner.create!(user: current_user, performance: @performance, day: @day)
-    @planner.performance.show.booked = true
+    # @planner.performance.show.booked = true
     redirect_to planner_path(day: @day)
   end
 
   def destroy
     @day = params[:day]
     @planner = Planner.find(params[:id])
-    @planner.performance.show.booked = false
+    # @planner.performance.show.booked = false
     @planner.destroy
     redirect_to planner_path(day: @day)
   end
