f77717b6be50a59202ed58ea22061e2247e2abcd
deelye <dee.yen.lye@gmail.com>
2020-03-14 22:07:26 +0000

    reordered code; removed unnecessary backslash

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'planner', to: 'pages#planner'
 
   root to: 'pages#home'
-  get 'contact', to: 'pages#contact'
-  get 'about', to: 'pages#about'
 
-  get 'shortlist', to: 'pages#shortlist'
   resources :shows, only: [:show, :index] do
     member do
       get 'follow'
@@ -14,12 +10,18 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :shortlist, only: [:show]
-
-  # should remove planner resources as going through pages now?
   resources :performances, only: [:index] do
     resources :planners, only: [:create]
   end
-  get '/planners', to: 'planners#index'
-  delete '/planners/:id', to: 'planners#destroy', as: :delete_planner
+
+  get 'shortlist', to: 'pages#shortlist'
+  resources :shortlist, only: [:show]
+
+  get 'planner', to: 'pages#planner'
+
+  get 'planners', to: 'planners#index'
+  delete 'planners/:id', to: 'planners#destroy', as: :delete_planner
+
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
 end
