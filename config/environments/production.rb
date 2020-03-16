fd090d778fefe7ea3b2ac9766590227b4a4dc4c5
deelye <dee.yen.lye@gmail.com>
2020-03-16 13:45:39 +0000

    changed email to username to match .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'myfringeplanner.com',
-    user_name:            ENV["GMAIL_EMAIL"],
+    user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
