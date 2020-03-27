498e9eb33709ce1d8a8e8c84258a3fcceaa807e6
deelye <dee.yen.lye@gmail.com>
2020-03-27 09:10:17 +0000

    allowed access to About without login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about ]
 
   def shortlist
     @follows = current_user.all_follows
